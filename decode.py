941d536bde002e1da77226356d81631556202146
ioanmh21 <ioanmichaelb@gmail.com>
2025-02-10 16:25:39 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,16 +1,77 @@
 import numpy as np
 import masks
 import decode_functions as df
+from PIL import Image
 
-mat=[]
-with open('input.txt','r') as file:
-    for line in file:
-        row = list(map(int, line.strip().split()))
-        mat.append(row)
+def png_to_binary_matrix(file_path, target_size=29):
+    img = Image.open(file_path).convert("L")
+    width, height = img.size
+    if width != height:
+        min_dim = min(width, height)
+        left = (width - min_dim) // 2
+        top = (height - min_dim) // 2
+        right = left + min_dim
+        bottom = top + min_dim
+        img = img.crop((left, top, right, bottom))
+
+    img_resized = img.resize((target_size, target_size), Image.NEAREST)
+
+    binary_img = img_resized.point(lambda x: 1 if x < 128 else 0, mode='1')
+
+    binary_matrix = []
+    for y in range(target_size):
+        row = []
+        for x in range(target_size):
+            pixel = binary_img.getpixel((x, y))
+            row.append(pixel)
+        binary_matrix.append(row)
+    return binary_matrix
+
+# mat=[]
+# with open('input.txt','r') as file:
+#     for line in file:
+#         row = list(map(int, line.strip().split()))
+#         mat.append(row)
+# mat=np.array(mat)
+
+# n=len(mat)
+# version = ((n - 21) // 4) + 1
+
+def display(mat):
+    scale = 20
+    scaled_matrix = np.kron(mat, np.ones((scale, scale)))
+
+    inverted_matrix=1-scaled_matrix
+
+    image = Image.fromarray(inverted_matrix * 255)
+    image.show()
+
+def debordare(mat):
+    def debord1(amat):
+        amat.pop(0)
+        amat.pop(-1)
+        bmat=[]
+        for line in amat:
+            line.pop(0)
+            line.pop(-1)
+            bmat.append(line)
+        return bmat
+    while mat[0][0]==0:
+        mat=debord1(mat)
+    return mat
+
+mat=png_to_binary_matrix('qrcode.png',target_size=31)
+display(mat)
+mat=debordare(mat)
+
+# g=open('output.txt','w')
+# for line in mat:
+#     for x in line:
+#         g.write(str(x)+' ')
+#     g.write('\n')
+# g.close()
+
 mat=np.array(mat)
-
-n=len(mat)
-version = ((n - 21) // 4) + 1
 
 data_mat=masks.get_data_matrix(mat)
 
